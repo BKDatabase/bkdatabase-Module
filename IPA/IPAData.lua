68c408f6800f959d28bd5af4b799b5cfd2e9aa09
Bapham <119475427+Bapham12@users.noreply.github.com>
2025-08-03 09:12:30 +0700

    Create IPAData.lua

---
+++ IPA/IPAData.lua
@@ -0,0 +1,699 @@
+return {
+	defaultLabel = "phát âm %s:",
+	labels = {
+		["lang"] = "%s:",
+		["pron"] = "phát âm",
+		["also"] = "also",
+		["alsolang"] = "also %s:",
+		["langalso"] = "%s also",
+		["local"] = "địa phương",
+		["localpron"] = "phát âm địa phương:",
+		["ipa"] = "IPA:",
+	},
+	defaultKey = "Trợ giúp:IPA",
+	langs = {
+		["und"] = {
+			text = "",
+			defaultLabelCode = "ipa",
+		},
+		["mis"] = {
+			text = "",
+			defaultLabelCode = "ipa",
+		},
+		["en"] = {
+			key = "Trợ giúp:IPA/tiếng Anh",
+			format = "/%s/",
+			defaultLabelCode = "",
+			dialects = {
+				["GB"] = {
+					text = "UK",
+					defaultLabelCode = "lang",
+				},
+				["UK"] = { aliasOf = "GB" },
+				["US"] = {
+					text = "UK",
+					defaultLabelCode = "lang",
+				},
+			},
+		},
+		["acw"] = {
+			name = "tiếng Ả Rập Hejaz",
+			key = "Trợ giúp:IPA/tiếng Ả Rập Hejaz",
+		},
+		["ady"] = {
+			key = "Trợ giúp:IPA/tiếng Adyghe",
+		},
+		["aeb"] = {
+			key = "Trợ giúp:IPA/tiếng Ả Rập Tunisia",
+		},
+		["aer"] = {
+			key = "Trợ giúp:IPA/thổ ngữ Úc",
+		},
+		["af"] = {
+			key = "Trợ giúp:IPA/tiếng Afrikaans",
+		},
+		["aln"] = {
+			key = "Trợ giúp:IPA/tiếng Albania",
+		},
+		["als"] = {
+			key = "Trợ giúp:IPA/tiếng Albania",
+		},
+		["am"] = {
+			key = "Trợ giúp:IPA/tiếng Amhara",
+		},
+		["ang"] = {
+			key = "Trợ giúp:IPA/tiếng Anh cổ",
+		},
+		["apc"] = {
+			dialects = {
+				["LB"] = {
+					name = "tiếng Ả Rập Lebanon",
+					key = "Trợ giúp:IPA/tiếng Ả Rập Lebanon",
+				},
+			},
+		},
+		["ar"] = {
+			key = "Trợ giúp:IPA/tiếng Ả Rập",
+		},
+		["arb"] = {
+			key = "Trợ giúp:IPA/tiếng Ả Rập",
+		},	
+		["arz"] = {
+			key = "Trợ giúp:IPA/tiếng Ả Rập Ai Cập",
+		},
+		["as"] = {
+			key = "Trợ giúp:IPA/tiếng Assam",
+		},
+		["ast"] = {
+			key = "Trợ giúp:IPA/tiếng Asturleonese",
+			dialects = {
+				["ES-LE"] = {
+					name = "tiếng León",
+				},
+			},
+		},
+		["aus"] = {
+			text = "thổ ngữ Úc",
+			key = "Trợ giúp:IPA/thổ ngữ Úc",
+		},
+		["az"] = {
+			key = "Trợ giúp:IPA/tiếng Azerbaijan",
+		},
+		["be"] = {
+			key = "Trợ giúp:IPA/tiếng Belarus",
+		},
+		["ber"] = {
+			key = "Trợ giúp:IPA/tiếng Berber",
+		},
+		["bg"] = {
+			key = "Trợ giúp:IPA/tiếng Bulgaria",
+		},
+		["bn"] = {
+			key = "Trợ giúp:IPA/tiếng Bengal",
+		},
+		["bo"] = {
+			text = "Tibetan",
+			key = "Trợ giúp:IPA/tiếng Tây Tạng",
+		},
+		["br"] = {
+			key = "Trợ giúp:IPA/tiếng Breton",
+		},
+		["bs"] = {
+			key = "Trợ giúp:IPA/tiếng Serbia-Croatia",
+		},
+		["ca"] = {
+			key = "Trợ giúp:IPA/tiếng Catalunya",
+			dialects = {
+				["ES-IB"] = {
+					name = "tiếng Catalunya Baleares",
+					key = "Trợ giúp:IPA/Tiếng Catalunya hải đảo",
+				},
+				["IT"] = {
+					name = "tiếng Alghero",
+					key = "Trợ giúp:IPA/Tiếng Catalunya hải đảo",
+				},
+				["VALENCIA"] = {
+					isVariant = true, -- makes it lowercase
+				},
+			},
+		},
+		["ckb"] = {
+			name = "tiếng Kurd miền trung",
+			key = "Trợ giúp:IPA/tiếng Kurd",
+		},
+		["cjy"] = {
+			name = "tiếng Tấn",
+			key = "Trợ giúp:IPA/tiếng Tấn",
+		},
+		["cmn"] = {
+			name = "Quan thoại",
+			link = "tiếng Trung chuẩn",
+			key = "Trợ giúp:IPA/Quan thoại",
+		},
+		["cnr"] = {
+			key = "Trợ giúp:IPA/tiếng Serbia-Croatia",
+		},
+		["co"] = {
+			key = "Trợ giúp:IPA/tiếng Corsica",
+		},
+		["crh"] = {
+			key = "Trợ giúp:IPA/tiếng Tatar",
+		},
+		["cs"] = {
+			key = "Trợ giúp:IPA/tiếng Séc",
+		},
+		["cy"] = {
+			key = "Trợ giúp:IPA/tiếng Wales",
+		},
+		["da"] = {
+			key = "Trợ giúp:IPA/tiếng Đan Mạch",
+		},
+		["de"] = {
+			key = "Trợ giúp:IPA/tiếng Đức chuẩn",
+			dialects = {
+				["AT"] = {},
+				["CH"] = {},
+			},
+		},
+		["dsb"] = {
+			key = "Trợ giúp:IPA/Sorbian",
+		},
+		["egl"] = {
+			key = "Trợ giúp:IPA/Emilian-Romagnol",
+		},
+		["el"] = {
+			key = "Trợ giúp:IPA/tiếng Hy Lạp",
+		},
+		["eo"] = {
+			key = "Trợ giúp:IPA/Quốc tế ngữ",
+		},
+		["es"] = {
+			key = "Trợ giúp:IPA/tiếng Tây Ban Nha",
+			dialects = {
+				["419"] = {
+					name = "tiếng Tây Ban Nha Mỹ Latinh",
+				},
+			},
+		},
+		["et"] = {
+			key = "Trợ giúp:IPA/tiếng Estonia",
+		},
+		["eu"] = {
+			key = "Trợ giúp:IPA/tiếng Basque",
+		},
+		["ext"] = {
+			key = "Trợ giúp:IPA/Astur-Leonese",
+		},
+		["fa"] = {
+			key = "Trợ giúp:IPA/tiếng Ba Tư",
+		},
+		["fi"] = {
+			key = "Trợ giúp:IPA/tiếng Phần Lan",
+		},
+		["fil"] = {
+			key = "Trợ giúp:IPA/tiếng Tagalog",
+		},
+		["fj"] = {
+			key = "Trợ giúp:IPA/tiếng Fiji",
+		},
+		["fr"] = {
+			key = "Trợ giúp:IPA/tiếng Pháp",
+		},
+		["frp"] = {
+			name = "tiếng Franco-Provençal",
+			key = "Trợ giúp:IPA/tiếng Franco-Provençal",
+		},
+		["fy"] = {
+			key = "Trợ giúp:IPA/tiếng Frisia tây",
+		},
+		["ga"] = {
+			key = "Trợ giúp:IPA/tiếng Ireland",
+			dialects = {
+				["GB"] = { aliasOf = "x-ulster" },
+				["IE-C"] = {
+					name = "tiếng Ireland Connacht",
+				},
+				["IE-M"] = {
+					name = "tiếng Ireland Munster",
+				},
+				["IE-U"] = { aliasOf = "x-ulster" },
+				["x-ulster"] = {
+					name = "tiếng Ireland Ulster",
+				},
+			}
+		},
+		["gct"] = {
+			key = "Trợ giúp:IPA/tiếng Đức Alemanni",
+		},
+		["gd"] = {
+			key = "Trợ giúp:IPA/tiếng Gael Scotland",
+		},
+		["gl"] = {
+			key = "Trợ giúp:IPA/tiếng Galicia",
+		},
+		["grc"] = {
+			key = "Trợ giúp:IPA/tiếng Hy Lạp",
+			dialects = {
+				["x-attic"] = {},
+				["x-classic"] = {},
+			},
+		},
+		["gsw"] = {
+			key = "Trợ giúp:IPA/tiếng Đức Alemanni",
+		},
+		["gu"] = {
+			key = "Trợ giúp:IPA/tiếng Gujarat",
+		},
+		["gv"] = {
+			key = "Trợ giúp:IPA/tiếng Manx",
+		},
+		["haw"] = {
+			key = "Trợ giúp:IPA/tiếng Hawaii",
+		},
+		["hbo"] = {
+			key = "Trợ giúp:IPA/tiếng Hebrew",
+		},
+		["he"] = {
+			key = "Trợ giúp:IPA/tiếng Hebrew",
+		},
+		["hi"] = {
+			key = "Trợ giúp:IPA/tiếng Hindi và Urdu",
+		},
+		["hmn"] = {
+			key = "Trợ giúp:IPA/tiếng H'Mông",
+		},
+		["hns"] = {
+			name = "tiếng Hindustan",
+			key = "Trợ giúp:IPA/tiếng Hindi và  Urdu",
+		},
+		["hr"] = {
+			key = "Trợ giúp:IPA/Serbo-Croatian",
+		},
+		["hsb"] = {
+			key = "Trợ giúp:IPA/Sorbian",
+		},
+		["hsn"] = {
+			key = "Trợ giúp:IPA/tiếng Tương",
+		},
+		["ht"] = {
+			key = "Trợ giúp:IPA/tiếng creole Haiti",
+		},
+		["hu"] = {
+			key = "Trợ giúp:IPA/tiếng Hungary",
+		},
+		["hy"] = {
+			key = "Trợ giúp:IPA/tiếng Armenia",
+		},
+		["id"] = {
+			key = "Trợ giúp:IPA/tiếng Mã Lai",
+		},
+		["ig"] = {
+			key = "Trợ giúp:IPA/tiếng Igbo",
+		},
+		["ik"] = {
+			key = "Trợ giúp:IPA/tiếng Inupiaq",
+		},
+		["is"] = {
+			key = "Trợ giúp:IPA/tiếng Iceland",
+		},
+		["it"] = {
+			key = "Trợ giúp:IPA/tiếng Ý",
+			dialects = {
+				["IT-52"] = {
+					name = "tiếng Tuscan",
+					key = "Trợ giúp:IPA/tiếng Ý miền trung",
+				},
+				["IT-55"] = {
+					name = "tiếng Ý miền trung",
+					key = "Trợ giúp:IPA/tiếng Ý miền trung",
+				},
+				["IT-57"] = {
+					name = "tiếng Ý miền trung",
+					key = "Trợ giúp:IPA/tiếng Ý miền trung",
+				},
+				["IT-RM"] = {
+					name = "tiếng Romanesco",
+					key = "Trợ giúp:IPA/tiếng Ý miền trung",
+				},
+			},
+		},
+		["iu"] = {
+			key = "Trợ giúp:IPA/tiếng Inuktitut",
+		},
+		["ja"] = {
+			key = "Trợ giúp:IPA/tiếng Nhật Bản",
+		},
+		["ka"] = {
+			key = "Trợ giúp:IPA/tiếng Gruzia",
+		},
+		["khb"] = {
+			name = "tiếng Tày Lự",
+			key = "Trợ giúp:IPA/tiếng Shan và Tày Lự",
+		},
+		["kjb"] = {
+			key = "Trợ giúp:IPA/tiếng Maya"
+		},
+		["kk"] = {
+			key = "Trợ giúp:IPA/tiếng Kazakh",
+		},
+		["kl"] = {
+			key = "Trợ giúp:IPA/tiếng Greenland",
+		},
+		["km"] = {
+			key = "Trợ giúp:IPA/tiếng Khmer",
+		},
+		["kmr"] = {
+			name = "Northern Kurdish",
+			key = "Trợ giúp:IPA/tiếng Kurd",
+		},
+		["ko"] = {
+			key = "Trợ giúp:IPA/tiếng Hàn",
+		},
+		["ksh"] = {
+			name = "Colognian",
+			key = "Trợ giúp:IPA/Colognian",
+		},
+		["ku"] = {
+			key = "Trợ giúp:IPA/tiếng Kurd",
+		},
+		["ky"] = {
+			key = "Trợ giúp:IPA/tiếng Kyrgyz",
+		},
+		["la"] = {
+			key = "Trợ giúp:IPA/tiếng Latinh",
+			dialects = {
+				["x-church"] = {
+					name = "Ecclesiastical Latin",
+				},
+				["x-classic"] = {
+					name = "tiếng Latinh cổ điển",
+				},
+			},
+		},
+		["lac"] = {
+			key = "Trợ giúp:IPA/tiếng Maya"
+		},
+		["lb"] = {
+			key = "Trợ giúp:IPA/tiếng Luxembourg",
+		},
+		["lij"] = {
+			key = "Trợ giúp:IPA/tiếng Liguria",
+		},
+		["lkt"] = {
+			key = "Trợ giúp:IPA/tiếng Lakota",
+		},
+		["lmo"] = {
+			key = "Trợ giúp:IPA/tiếng Lombard",
+		},
+		["lo"] = {
+			key = "Trợ giúp:IPA/tiếng Lào",
+		},
+		["lt"] = {
+			key = "Trợ giúp:IPA/tiếng Litva",
+		},
+		["lv"] = {
+			key = "Trợ giúp:IPA/Latvian",
+		},
+		["mfe"] = {
+			name = "Mauritian Creole",
+			key = "Trợ giúp:IPA/Mauritian Creole",
+		},
+		["mg"] = {
+			key = "Trợ giúp:IPA/Malagasy",
+		},
+		["mh"] = {
+			key = "Trợ giúp:IPA/Marshallese",
+		},
+		["mi"] = {
+			key = "Trợ giúp:IPA/tiếng Māori",
+		},
+		["mk"] = {
+			key = "Trợ giúp:IPA/tiếng Macedonia",
+		},
+		["ml"] = {
+			key = "Trợ giúp:IPA/Malayalam",
+		},
+		["mn"] = {
+			key = "Trợ giúp:IPA/tiếng Mông Cổ",
+		},
+		["mr"] = {
+			key = "Trợ giúp:IPA/Marathi",
+		},
+		["ms"] = {
+			key = "Trợ giúp:IPA/tiếng Mã Lai",
+		},
+		["mt"] = {
+			key = "Trợ giúp:IPA/Maltese",
+		},
+		["mwl"] = {
+			key = "Trợ giúp:IPA/Astur-Leonese",
+		},
+		["my"] = {
+			key = "Trợ giúp:IPA/tiếng Miến Điện",
+		},
+		["myn"] = {
+			key = "Trợ giúp:IPA/tiếng Maya",
+		},
+		["nah"] = {
+			key = "Trợ giúp:IPA/tiếng Nahuatl",
+		},
+		["nan"] = {
+			dialects = {
+				["TW"] = {
+					key = "Trợ giúp:IPA/Taiwanese Hokkien",
+				},
+			},
+		},
+		["nap"] = {
+			key = "Trợ giúp:IPA/Neapolitan",
+		},
+		["nci"] = {
+			key = "Trợ giúp:IPA/tiếng Nahuatl",
+		},
+		["ne"] = {
+			key = "Trợ giúp:IPA/tiếng Nepal",
+		},
+		["nhe"] = {
+			name = "tiếng Nahuatl Huastec",
+			key = "Trợ giúp:IPA/tiếng Nahuatl",
+		},
+		["nl"] = {
+			key = "Trợ giúp:IPA/tiếng Hà Lan",
+			dialects = {
+				["BE"] = {},
+			},
+		},
+		["nlv"] = {
+			key = "Trợ giúp:IPA/tiếng Nahuatl",
+		},
+		["no"] = {
+			key = "Trợ giúp:IPA/tiếng Na Uy",
+			dialects = {
+				["NO-03"] = {
+					name = "tiếng Na Uy đô thị đông",
+				},
+			}
+		},
+		["nod"] = {
+			key = "Trợ giúp:IPA/tiếng Thái bắc",
+		},
+		["nv"] = {
+			key = "Trợ giúp:IPA/tiếng Navajo",
+		},
+		["oc"] = {
+			key = "Trợ giúp:IPA/tiếng Occitan",
+		},
+		["or"] = {
+			key = "Trợ giúp:IPA/Odia",
+		},
+		["pa"] = {
+			key = "Trợ giúp:IPA/tiếng Punjab",
+		},
+		["pht"] = {
+			key = "Trợ giúp:IPA/tiếng Lào",
+		},
+		["pi"] = {
+			key = "Trợ giúp:IPA/Sanskrit",
+		},
+		["pjt"] = {
+			key = "Trợ giúp:IPA/Australian languages",
+		},
+		["pl"] = {
+			key = "Trợ giúp:IPA/tiếng Ba Lan",
+		},
+		["pms"] = {
+			key = "Trợ giúp:IPA/tiếng Piedmonte",
+		},
+		["prs"] = {
+			key = "Trợ giúp:IPA/tiếng Ba Tư",
+		},
+		["pt"] = {
+			key = "Trợ giúp:IPA/tiếng Bồ Đào Nha",
+			dialects = {
+				["BR"] = {
+					name = "tiếng Bồ Đào Nha Brazil",
+				},
+				["PT"] = {
+					name = "tiếng Bồ Đào Nha Âu",
+				},
+			}
+		},
+		["qu"] = {
+			key = "Trợ giúp:IPA/tiếng Quechua",
+		},
+		["rgn"] = {
+			key = "Trợ giúp:IPA/Emilian-Romagnol",
+		},
+		["rm"] = {
+			key = "Trợ giúp:IPA/tiếng Romansh",
+		},
+		["ro"] = {
+			key = "Trợ giúp:IPA/tiếng Rumani",
+		},
+		["ru"] = {
+			key = "Trợ giúp:IPA/tiếng Nga",
+		},
+		["sa"] = {
+			key = "Trợ giúp:IPA/tiếng Phạn",
+		},
+		["sc"] = {
+			key = "Trợ giúp:IPA/tiếng Sardinia",
+		},
+		["scn"] = {
+			key = "Trợ giúp:IPA/tiếng Sicilia",
+			dialects = {
+				["IT-75"] = {
+					name = "Salentino",
+				},
+				["IT-78"] = {
+					name = "Calabrian",
+					link = "Central-Southern Calabrian",
+				},
+			},
+		},
+		["sdh"] = {
+			key = "Trợ giúp:IPA/tiếng Kurd",
+		},
+		["sh"] = {
+			key = "Trợ giúp:IPA/Serbo-Croatian",
+		},
+		["shn"] = {
+			key = "Trợ giúp:IPA/tiếng Shan và Tày Lự",
+		},
+		["sk"] = {
+			key = "Trợ giúp:IPA/tiếng Slovak",
+		},
+		["sl"] = {
+			key = "Trợ giúp:IPA/tiếng Slovenia",
+		},
+		["sou"] = {
+			key = "Trợ giúp:IPA/tiếng Thái",
+		},
+		["sq"] = {
+			key = "Trợ giúp:IPA/tiếng Albania",
+		},
+		["sr"] = {
+			key = "Trợ giúp:IPA/Serbo-Croatian",
+		},
+		["ss"] = {
+			key = "Trợ giúp:IPA/Nguni",
+		},
+		["sv"] = {
+			key = "Trợ giúp:IPA/tiếng Thụy Điển",
+			dialects = {
+				["FI"] = {
+					name = "tiếng Thụy Điển Phần Lan",
+				},
+			},
+		},
+		["sw"] = {
+			key = "Trợ giúp:IPA/tiếng Swahili",
+		},
+		["swg"] = {
+			key = "Trợ giúp:IPA/Alemannic German",
+		},
+		["syl"] = {
+			key = "Trợ giúp:IPA/Sylheti",
+		},
+		["ta"] = {
+			key = "Trợ giúp:IPA/tiếng Tamil",
+		},
+		["te"] = {
+			key = "Trợ giúp:IPA/Telugu",
+		},
+		["tg"] = {
+			key = "Trợ giúp:IPA/tiếng Ba Tư",
+		},
+		["th"] = {
+			key = "Trợ giúp:IPA/tiếng Thái",
+		},
+		["ti"] = {
+			key = "Trợ giúp:IPA/Tigrinya",
+		},
+		["tk"] = {
+			key = "Trợ giúp:IPA/tiếng Turkmen",
+		},
+		["tl"] = {
+			key = "Trợ giúp:IPA/tiếng Tagalog",
+		},
+		["tr"] = {
+			key = "Trợ giúp:IPA/tiếng Thổ Nhĩ Kỳ",
+		},
+		["tt"] = {
+			key = "Trợ giúp:IPA/tiếng Tatar",
+		},
+		["tts"] = {
+			name = "tiếng Isan",
+			key = "Trợ giúp:IPA/tiếng Lào",
+		},
+		["uk"] = {
+			key = "Trợ giúp:IPA/tiếng Ukraina",
+		},
+		["ur"] = {
+			key = "Trợ giúp:IPA/tiếng Hindi và Urdu",
+		},
+		["uz"] = {
+			key = "Trợ giúp:IPA/tiếng Uzbek",
+		},
+		["vec"] = {
+			key = "Trợ giúp:IPA/tiếng Venice",
+		},
+		["vi"] = {
+			key = "Trợ giúp:IPA/tiếng Việt",
+		},
+		["wa"] = {
+			key = "Trợ giúp:IPA/tiếng Walloon",
+		},
+		["wae"] = {
+			key = "Trợ giúp:IPA/tiếng Đức Alemanni",
+		},
+		["wbp"] = {
+			key = "Trợ giúp:IPA/thổ ngữ Úc",
+		},
+		["wuu"] = {
+			key = "Trợ giúp:IPA/tiếng Ngô",
+		},
+		["xh"] = {
+			key = "Trợ giúp:IPA/tiếng Nguni",
+		},
+		["yi"] = {
+			key = "Trợ giúp:IPA/tiếng Yiddish",
+		},
+		["yo"] = {
+			key = "Trợ giúp:IPA/tiếng Yoruba",
+		},
+		["yue"] = {
+			name = "tiếng Quảng Đông",
+			key = "Trợ giúp:IPA/tiếng Quảng Đông",
+		},
+		["za"] = {
+			key = "Trợ giúp:IPA/tiếng Choang",
+		},
+		["zsm"] = {
+			key = "Trợ giúp:IPA/tiếng Mã Lai",
+		},
+		["zu"] = {
+			key = "Trợ giúp:IPA/tiếng Nguni",
+		},
+	},
+}
