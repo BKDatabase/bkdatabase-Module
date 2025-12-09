ad19f3a8065b6d89ec6114449c75fd62012b825d
Bapham <119475427+Bapham12@users.noreply.github.com>
2025-12-09 17:21:22 +0700

    Add French localization messages for Wikibase

---
+++ Wikibase/Wikibasei18n.lua
@@ -0,0 +1,40 @@
+return {
+-- Error message
+	["property-param-not-provided"] = "paramètre 'property' manquant",
+	["qualifier-param-not-provided"] = "paramètre 'qualifier' manquant",
+	["entity-param-not-provided"] = "paramètre 'entity' manquant",
+	["entity-not-found"] = "entité non trouvée",
+	["unknown-claim-type"] = "type d'affirmation inconnu",
+	["unknown-snak-typeg"] = "type de 'snak' inconnu",
+	["unknown-datavalue-type"] = "type de valeur de donnée inconnu",
+	["unknown-entity-type"] = "type d'entité inconnu",
+	["invalid-id"] = "identifiant inconnu",
+	["invalid-datatype"] = "donnée de type $1, devrait être $2",
+	["invalid-linktype"] = "type de lien inconnu",
+	["invalid-event-type"] = "l'événement devrait être une chaîne, est $1",
+	["invalid-entity-id"] = "identifiant d'entité invalide : $1",
+	["invalid-project-code"] = "identifiant de project Wikimédia invalide : $1",
+	["cat-unsorted-issue"] = "Page avec un problème de codage Wikidata",
+	
+-- Autres messages
+	["no-label"] = "pas de libellé",
+	['no description'] = "pas de description",
+	["novalue"] = "-",
+	["hidevalue"] = "-", -- symbol signalling in the wikitext that the value should not be displayed
+	["somevalue"] = "inconnu",
+	["sometime"] = "date inconnue",
+	['to translate'] = 'Page utilisant des données de Wikidata à traduire',
+	["trackingcat"] = 'Page utilisant $1',
+	['see-wikidata-value'] = "Voir et modifier les données sur Wikidata", -- lien vers des valeurs sur une propriété Wikidata
+	['see-wikidata'] = "Voir l'élément Wikidata correspondant", -- utilisé sur les liens Wikidata par défaut en l'absence de lien interne disponible
+	['see-another-project'] = "Article sur $1",
+	['see-another-language'] = "Article sur Wikipédia en $1",
+	['activate-query'] = "<div class='par wikidata'></div>", -- activates an on-demand-only query
+	['qualif-separator'] = " ; ", -- separate qualifiers shown next to a standard statement
+
+
+	['approximate-value'] = "environ $1",
+	['approximate-place'] = "$1 (ou environs)",
+	['uncertain-information'] = "$1 (?)",
+
+}
