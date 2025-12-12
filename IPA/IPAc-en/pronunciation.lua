971e2c8678b9b30d573c61be5db2a6f13dc36f4a
Bapham <119475427+Bapham12@users.noreply.github.com>
2025-12-12 15:35:55 +0700

    Create pronunciation.lua

---
+++ IPA/IPAc-en/pronunciation.lua
@@ -0,0 +1,36 @@
+-- This module contains pronunciation-key data for [[Module:IPAc-en]].
+
+return {
+	{
+		code = 'lang',
+		text = 'tiếng Anh:'
+	},
+	{
+		code = 'pron',
+		text = ''
+	},
+	{
+		code = 'local',
+		text = 'địa phương'
+	},
+	{
+		code = 'ipa',
+		text = '[[w:vi:Bảng mẫu tự ngữ âm quốc tế|IPA]]:'
+	},
+	{
+		code = 'uk',
+		text = '[[w:vi:Tiếng Anh của người Anh|UK]]:'
+	},
+	{
+		code = 'us',
+		text = '[[w:vi:Tiếng Anh Mỹ|US]]:'
+	},
+	{
+		code = 'uklang',
+		text = '[[w:vi:Tiếng Anh của người Anh|Tiếng Anh-Anh]]:'
+	},
+	{
+		code = 'uslang',
+		text = '[[w:vi:Tiếng Anh Mỹ|Tiếng Anh-Mỹ]]:'
+	},
+}
