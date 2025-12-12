1cbd5a2f2de1eab6b4826b93fbaaf8ea13c1d23b
Bapham <119475427+Bapham12@users.noreply.github.com>
2025-12-12 15:35:27 +0700

    Create phonemes.lua

---
+++ IPA/IPAc-en/phonemes.lua
@@ -0,0 +1,554 @@
+-- This module contains phoneme data for [[Module:IPAc-en]].
+
+return {
+	-- CONSONANTS
+	{
+		code = "b",
+		label = "b",
+		tooltip = "'b' trong 'buy'",
+	},
+	{
+		code = "d",
+		label = "d",
+		tooltip = "'d' trong 'dye'",
+	},
+	{
+		code = "dj",
+		aliases = {"dy", "dʲ"},
+		label = "dj",
+		tooltip = "/dj/: 'd' trong 'dew'",
+	},
+	{
+		code = "dʒ",
+		aliases = {"dZ", "ʤ"},
+		label = "dʒ",
+		tooltip = "/dʒ/: 'j' trong 'jam'",
+	},
+	{
+		code = "ð",
+		aliases = {"D"},
+		label = "ð",
+		tooltip = "/ð/: 'th' trong 'thy'",
+	},
+	{
+		code = "f",
+		label = "f",
+		tooltip = "'f' trong 'find'",
+	},
+	{
+		code = "ɡ",
+		aliases = {"g"},
+		label = "ɡ",
+		tooltip = "/ɡ/: 'g' trong 'guy'",
+	},
+	{
+		code = "h",
+		label = "h",
+		tooltip = "'h' trong 'hi'",
+	},
+	{
+		code = "hw",
+		aliases = {"ʍ"},
+		label = "hw",
+		tooltip = "/hw/: 'wh' trong 'why'",
+	},
+	{
+		code = "j",
+		aliases = {"y"},
+		label = "j",
+		tooltip = "/j/: 'y' trong 'yes'",
+	},
+	{
+		code = "k",
+		label = "k",
+		tooltip = "'k' trong 'kind'",
+	},
+	{
+		code = "l",
+		label = "l",
+		tooltip = "'l' trong 'lie'",
+	},
+	{
+		code = "lj",
+		aliases = {"ly", "lʲ"},
+		label = "lj",
+		tooltip = "/lj/: 'l' trong 'lute'",
+	},
+	{
+		code = "m",
+		label = "m",
+		tooltip = "'m' trong 'my'",
+	},
+	{
+		code = "n",
+		label = "n",
+		tooltip = "'n' trong 'nigh'",
+	},
+	{
+		code = "nj",
+		aliases = {"ny", "nʲ"},
+		label = "nj",
+		tooltip = "/nj/: 'n' trong 'new'",
+	},
+	{
+		code = "ŋ",
+		aliases = {"N", "ng"},
+		label = "ŋ",
+		tooltip = "/ŋ/: 'ng' trong 'sing'",
+	},
+	{
+		code = "p",
+		label = "p",
+		tooltip = "'p' trong 'pie'",
+	},
+	{
+		code = "r",
+		aliases = {"ɹ", "ɻ"},
+		label = "r",
+		tooltip = "'r' trong 'rye'",
+	},
+	{
+		code = "s",
+		label = "s",
+		tooltip = "'s' trong 'sigh'",
+	},
+	{
+		code = "sj",
+		aliases = {"sy", "sʲ"},
+		label = "sj",
+		tooltip = "/sj/: 's' trong 'consume'",
+	},
+	{
+		code = "ʃ",
+		aliases = {"S", "sh"},
+		label = "ʃ",
+		tooltip = "/ʃ/: 'sh' trong 'shy'",
+	},
+	{
+		code = "t",
+		label = "t",
+		tooltip = "'t' trong 'tie'",
+	},
+	{
+		code = "tj",
+		aliases = {"ty", "tʲ"},
+		label = "tj",
+		tooltip = "/tj/: 't' trong 'tune'",
+	},
+	{
+		code = "tʃ",
+		aliases = {"tS", "ʧ", "ch"},
+		label = "tʃ",
+		tooltip = "/tʃ/: 'ch' trong 'China'",
+	},
+	{
+		code = "θ",
+		aliases = {"T"},
+		label = "θ",
+		tooltip = "/θ/: 'th' trong 'thigh'",
+	},
+	{
+		code = "θj",
+		aliases = {"θy", "θʲ"},
+		label = "θj",
+		tooltip = "/θj/: 'th' trong 'enthuse'",
+	},
+	{
+		code = "v",
+		label = "v",
+		tooltip = "'v' trong 'vie'",
+	},
+	{
+		code = "w",
+		label = "w",
+		tooltip = "'w' trong 'wind'",
+	},
+	{
+		code = "z",
+		label = "z",
+		tooltip = "'z' trong 'zoom'",
+	},
+	{
+		code = "zj",
+		aliases = {"zy", "zʲ"},
+		label = "zj",
+		tooltip = "/zj/: 'Z' trong 'Zeus'",
+	},
+	{
+		code = "ʒ",
+		aliases = {"Z", "zh"},
+		label = "ʒ",
+		tooltip = "/ʒ/: 's' trong 'pleasure'",
+	},
+	-- VOWELS
+	{
+		code = "ɑː",
+		aliases = {"A:", "ɑ:", "aː", "a:"},
+		label = "ɑː",
+		tooltip = "/ɑː/: 'a' trong 'father'",
+	},
+	{
+		code = "ɑːr",
+		aliases = {"ɑɹ", "aːr", "a:r", "ɑ:r", "ɑr"},
+		label = "ɑːr",
+		tooltip = "/ɑːr/: 'ar' trong 'far'",
+	},
+	{
+		code = "ɒ",
+		aliases = {"Q"},
+		label = "ɒ",
+		tooltip = "/ɒ/: 'o' trong 'body'",
+	},
+	{
+		code = "ɒr",
+		aliases = {"Qr"},
+		label = "ɒr",
+		tooltip = "/ɒr/: 'or' trong 'moral'",
+	},
+	{
+		code = "æ",
+		aliases = {"ae", "{"},
+		label = "æ",
+		tooltip = "/æ/: 'a' trong 'bad'",
+	},
+	{
+		code = "ær",
+		aliases = {"aer", "&r", "æɹ"},
+		label = "ær",
+		tooltip = "/ær/: 'arr' trong 'marry'",
+	},
+	{
+		code = "aɪ",
+		aliases = {"ai", "aI"},
+		label = "aɪ",
+		tooltip = "/aɪ/: 'i' trong 'tide'",
+	},
+	{
+		code = "aɪər",
+		aliases = {"aɪr", "aIr", "aɪə"},
+		label = "aɪər",
+		tooltip = "/aɪər/: 'ire' trong 'fire'",
+	},
+	{
+		code = "aʊ",
+		aliases = {"au", "aU"},
+		label = "aʊ",
+		tooltip = "/aʊ/: 'ou' trong 'mouth'",
+	},
+	{
+		code = "aʊər",
+		aliases = {"aʊr", "aUr", "aʊə", "aur"},
+		label = "aʊər",
+		tooltip = "/aʊər/: 'our' trong 'hour'",
+	},
+	{
+		code = "ɛ",
+		aliases = {"E"},
+		label = "ɛ",
+		tooltip = "/ɛ/: 'e' trong 'dress'",
+	},
+	{
+		code = "ɛr",
+		aliases = {"Er"},
+		label = "ɛr",
+		tooltip = "/ɛr/: 'err' trong 'merry'",
+	},
+	{
+		code = "eɪ",
+		aliases = {"ei", "eI", "eː", "e:"},
+		label = "eɪ",
+		tooltip = "/eɪ/: 'a' trong 'face'",
+	},
+	{
+		code = "ɛər",
+		aliases = {"eir", "eIr", "e@r", "E@r", "ɛɪɹ", "eɪr", "eːr", "e:r", "ɛəɹ", "ɛə", "ɛːr", "ɛ:r", "ɛː", "ɛ:", "E:", "E:r"},
+		label = "ɛər",
+		tooltip = "/ɛər/: 'are' trong 'bare'",
+	},
+	{
+		code = "ɛəˈr",
+		aliases = {"ɛə'r", "eəˈr", "eə'r", "e@'r", "E@'r", "ɛːˈr", "E:'r"},
+		label = "ɛəˈr",
+		tooltip = "/ɛəˈr/: 'ere' trong 'thereof'",
+	},
+	{
+		code = "ɛəˌr",
+		aliases = {"ɛə,r", "eəˌr", "eə,r", "E@,r", "E@%r", "e@,r", "e@%r", "ɛːˌr", "E:,r", "E:%r"},
+		label = "ɛəˌr",
+		tooltip = "/ɛəˌr/: 'ere' trong 'thereof'",
+	},
+	{
+		code = "ɪ",
+		aliases = {"I", "ᵻ"},
+		label = "ɪ",
+		tooltip = "/ɪ/: 'i' trong 'kit'",
+	},
+	{
+		code = "ɪr",
+		label = "ɪr",
+		tooltip = "/ɪr/: 'irr' trong 'mirror'",
+	},
+	{
+		code = "iː",
+		aliases = {"i:"},
+		label = "iː",
+		tooltip = "/iː/: 'ee' trong 'fleece'",
+	},
+	{
+		code = "ɪər",
+		aliases = {"i:r", "iːr", "I@r", "i@r", "ɪəɹ", "iːɹ", "ɪə"},
+		label = "ɪər",
+		tooltip = "/ɪər/: 'ear' trong 'near'",
+	},
+	{
+		code = "ɪəˈr",
+		aliases = {"ɪə'r", "I@'r"},
+		label = "ɪəˈr",
+		tooltip = "/ɪəˈr/: 'er' trong 'spheroidal'",
+	},
+	{
+		code = "ɪəˌr",
+		aliases = {"ɪə,r", "I@,r", "I@%r"},
+		label = "ɪəˌr",
+		tooltip = "/ɪəˌr/: 'er' trong 'spheroidal'",
+	},
+	{
+		code = "oʊ",
+		aliases = {"o:", "oː", "ou", "oU", "@u", "@U", "əʊ", "əu", "oʊ-"},
+		label = "oʊ",
+		tooltip = "/oʊ/: 'o' trong 'code'",
+	},
+	{
+		code = "ɔː",
+		aliases = {"O", "O:", "ɔ:", "ɒː", "Q:"},
+		label = "ɔː",
+		tooltip = "/ɔː/: 'au' trong 'fraud'",
+	},
+	{
+		code = "ɔːr",
+		aliases = {"Or", "ɔɹ", "ɔ(r)", "ɔr", "ɔər", "oUr", "ɔəɹ", "ɔʊɹ", "oʊɹ", "oʊr", "oːr", "o:r", "ɔə", "ɔə(r)"},
+		label = "ɔːr",
+		tooltip = "/ɔːr/: 'ar' trong 'war'",
+	},
+	{
+		code = "ɔɪ",
+		aliases = {"oj", "ɔj", "oi", "oɪ", "ɔi", "OI", "oy"},
+		label = "ɔɪ",
+		tooltip = "/ɔɪ/: 'oi' trong 'choice'",
+	},
+	{
+		code = "ɔɪər",
+		aliases = {"ɔɪr", "oyr", "ɔɪə"},
+		label = "ɔɪər",
+		tooltip = "/ɔɪər/: 'oir' trong 'loir'",
+	},
+	{
+		code = "ʊ",
+		aliases = {"U", "ᵿ"},
+		label = "ʊ",
+		tooltip = "/ʊ/: 'u' trong 'push'",
+	},
+	{
+		code = "ʊr",
+		aliases = {"Ur"},
+		label = "ʊr",
+		tooltip = "/ʊr/: 'our' trong 'courier'",
+	},
+	{
+		code = "uː",
+		aliases = {"u:"},
+		label = "uː",
+		tooltip = "/uː/: 'oo' trong 'goose'",
+	},
+	{
+		code = "ʊər",
+		aliases = {"u:r", "uːr", "u@r", "U@r", "ʊəɹ", "ʊə"},
+		label = "ʊər",
+		tooltip = "/ʊər/: 'our' trong 'tour'",
+	},
+	{
+		code = "ʊəˈr",
+		aliases = {"ʊə'r", "U@'r"},
+		label = "ʊəˈr",
+		tooltip = "/ʊəˈr/: 'ur' trong 'plurality'",
+	},
+	{
+		code = "ʊəˌr",
+		aliases = {"ʊə,r", "U@,r", "U@%r"},
+		label = "ʊəˌr",
+		tooltip = "/ʊəˌr/: 'ur' trong 'plurality'",
+	},
+	{
+		code = "ʌ",
+		aliases = {"V"},
+		label = "ʌ",
+		tooltip = "/ʌ/: 'u' trong 'cut'",
+	},
+	{
+		code = "ʌr",
+		aliases = {"Vr", "ʌɹ"},
+		label = "ʌr",
+		tooltip = "/ʌr/: 'urr' trong 'hurry'",
+	},
+	{
+		code = "ɜːr",
+		aliases = {"ɝː", "ɝ", "3r", "3:r", "əːr", "@:r", "ɜɹ", "ɜ(r)", "ɜr"},
+		label = "ɜːr",
+		tooltip = "/ɜːr/: 'ur' trong 'fur'",
+	},
+	{
+		code = "ə",
+		aliases = {"@"},
+		label = "ə",
+		tooltip = "/ə/: 'a' trong 'about'",
+	},
+	-- SYLLABIC CONSONANTS
+	{
+		code = "ər",
+		aliases = {"ɚ", "@r", "əɹ", "ə(r)"},
+		label = "ər",
+		tooltip = "/ər/: 'er' trong 'letter'",
+	},
+	{
+		code = "əl",
+		aliases = {"@l", "ᵊl", "l̩"},
+		label = "əl",
+		tooltip = "/əl/: 'le' trong 'bottle'",
+	},
+	{
+		code = "ən",
+		aliases = {"@n", "ᵊn", "n̩"},
+		label = "ən",
+		tooltip = "/ən/: 'on' trong 'button'",
+	},
+	{
+		code = "əm",
+		aliases = {"@m", "ᵊm", "m̩"},
+		label = "əm",
+		tooltip = "/əm/: 'm' trong 'rhythm'",
+	},
+	-- ARCHIPHONEMES
+	{
+		code = "i",
+		label = "i",
+		tooltip = "/i/: 'y' trong 'happy'",
+	},
+	{
+		code = "u",
+		label = "u",
+		tooltip = "/u/: 'u' trong 'influence'",
+	},
+	-- MARGINAL SEGMENTS
+	{
+		code = "x",
+		label = "x",
+		aliases = {"kh"},
+		tooltip = "/x/: 'ch' trong 'loch'",
+	},
+	{
+		code = "ʔ",
+		aliases = {"?"},
+		label = "ʔ",
+		tooltip = "/ʔ/: the catch in 'uh-oh'",
+	},
+	{
+		code = "ɒ̃",
+		aliases = {"ɑ̃", "ɒ~", "ɑ~", "Q~"},
+		label = "ɒ̃",
+		tooltip = "/ɒ̃/: nasal 'an' trong 'vin blanc'",
+	},
+	{
+		code = "æ̃",
+		aliases = {"ae~", "{~", "ã", "a~"},
+		label = "æ̃",
+		tooltip = "/æ̃/: nasal 'in' trong 'vin blanc'",
+	},
+	{
+		code = "ɜː",
+		aliases = {"3", "3:", "ɜ:", "ɜ", "əː", "@:"},
+		label = "ɜː",
+		tooltip = "/ɜː/: r-less 'ur' trong 'nurse'",
+	},
+	-- SUPRASEGMENTALS
+	{
+		code = "ˈ",
+		aliases = {"'", '"'},
+		label = "ˈ",
+		tooltip = "/ˈ/: primary stress follows",
+	},
+	{
+		code = "ˌ",
+		aliases = {",", "%"},
+		label = "ˌ",
+		tooltip = "/ˌ/: secondary stress follows",
+	},
+	{
+		code = ".",
+		aliases = {"·"},
+		label = ".",
+		tooltip = "/./: syllable break",
+	},
+	{
+		code = "#",
+		label = "#",
+		tooltip = "/#/: morpheme break",
+	},
+	{
+		code = "!",
+		label = "&#124;",
+		tooltip = "/|/: prosodic break (minor)",
+	},
+	{
+		code = "!!",
+		aliases = {"‖"},
+		label = "‖",
+		tooltip = "/‖/: prosodic break (major)",
+	},
+	-- SEPARATORS
+	-- Tables without tooltip text are used as word separators. In addition to
+	-- not having any tooltip text in the template output, they are not
+	-- underlined.
+	{
+		code = "_",
+		label = " <wbr/>",
+	},
+	{
+		code = "nbsp",
+		aliases = {"&nbsp;"},
+		label = " ",
+	},
+	{
+		code = ",_",
+		label = ", <wbr/>",
+	},
+	{
+		code = "-",
+		aliases = {"–"},
+		label = "-",
+	},
+	{
+		code = "(",
+		label = "(",
+	},
+	{
+		code = ")",
+		label = ")",
+	},
+	-- This is useful for highly variable words in place names, which are not
+	-- important to transcribe.
+	{
+		code = "...",
+		label = "&thinsp;...&thinsp;<wbr/>",
+	},
+	-- Obsolete combinations, kept for compatibility
+	{
+		code = "juː",
+		label = "juː",
+		tooltip = "/juː/: 'u' trong 'cute'",
+	},
+	{
+		code = "jʊər",
+		label = "jʊər",
+		tooltip = "/jʊər/: 'ure' trong 'cure'",
+	},
+}
