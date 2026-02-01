7f38517db31477ef7316dd1074d68f2ce836fee9
Tphamtranba <119475427+Bapham12@users.noreply.github.com>
2026-02-01 09:53:47 +0700

    Create M.lua for political party data
    Add constant data for the Montecaran Socialist Party

---
+++ Political party/Data/M.lua
@@ -0,0 +1,13 @@
+-- Constant data used by [[Module:Political party]]
+
+local alternate = {
+}
+
+local full = {
+	["Montecaran Socialist Party"] = {abbrev = "PSM", color = "#E3170D", shortname = "",},
+}
+
+return {
+	full = full,
+	alternate = alternate,
+}
