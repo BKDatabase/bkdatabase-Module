e6ab0f8df49f80cefa13a4a8b46433cc9cd32d3f
Tphamtranba <119475427+Bapham12@users.noreply.github.com>
2026-02-01 09:53:21 +0700

    Add Lua module for political party data

---
+++ Political party/Data/L.lua
@@ -0,0 +1,14 @@
+-- Constant data used by [[Module:Political party]]
+
+local alternate = {
+}
+
+local full = {
+	["Liberal Party – The Radicals"] = {abbrev = "PL-R", color = "#F5B82E", shortname = "Liberal-Radicals",},
+	["Líga Zararcía"] = {abbrev = "LZ", color = "#FF4F00", shortname = "Líga Zararcía",},
+}
+
+return {
+	full = full,
+	alternate = alternate,
+}
