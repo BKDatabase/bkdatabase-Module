5f87df4df243e4708b0c8f6d6f2258db6f67ea54
Tphamtranba <119475427+Bapham12@users.noreply.github.com>
2026-02-01 09:57:16 +0700

    Add Q.lua for political party data

---
+++ Political party/Data/Q.lua
@@ -0,0 +1,21 @@
+-- Constant data used by [[Module:Political party]]
+
+local alternate = {
+	["QWP"] = "Qaumi Watan Party",
+}
+
+local full = {
+	["Qaransoor Party"] = {abbrev = "", color = "#39B9EB", shortname = "",},
+	["Qaumi Watan Party"] = {abbrev = "QWP", color = "#b30029", shortname = "",},
+	["Quami Ekta Dal"] = {abbrev = "QED", color = "green", shortname = "",},
+	["Queensland Greens"] = {abbrev = "", color = "#39B54A", shortname = "Greens",},
+	["Queensland Labor Party"] = {abbrev = "", color = "#008080", shortname = "Queensland Labor",},
+	["Queremos Puerto"] = {abbrev = "", color = "#99BE19", shortname = "",},
+	["Quintuple Coalition"] = {abbrev = "QC", color = "white", shortname = "",},
+	["Quisqueyano Christian Democratic Party"] = {abbrev = "", color = "#BB0000", shortname = "",},
+}
+
+return {
+	full = full,
+	alternate = alternate,
+}
